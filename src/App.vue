245f8abf2ab9c2c1aa32445bce5387dac3c44f77
WDEcho <1207724603@qq.com>
2023-02-21 17:44:15 +0800

    测试提交

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
+  <p>首页</p>
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-cell-group>
+    <van-cell title="单元格" value="内容" />
+    <van-cell title="单元格" value="内容" label="描述信息" />
+</van-cell-group>
+<div class="box"></div>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="less">
+.box{
+  width: 100px;
+  height: 100px;
+  background-color: red;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+// 加载Vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+// 加载全局样式
+import './styles/index.less'
+// 动态设置rem 基准值
+import 'amfe-flexible'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Vant).use(store).use(router).mount('#app')
